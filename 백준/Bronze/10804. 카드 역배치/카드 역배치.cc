f7c8dfc8389a9ef4a10e31c89f1eaa72e43a5913
ZergRocks <139047613+ZergRocks@users.noreply.github.com>
2024-08-23 23:18:09 +0900

    [Bronze II] Title: 카드 역배치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10804. 카드 역배치/카드 역배치.cc
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.cc
@@ -1,27 +1,24 @@
-#include <algorithm>
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+int num[21];
 
-  int n = 21;
-  int cards[n];
+void reverse(int a, int b){
+  for(int i = 0; i < (b - a + 1) / 2; i++)
+    swap(num[a+i], num[b-i]);
+}
 
-  for (int i = 0; i < n; i++) {
-    cards[i] = i;
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+    
+  for(int i = 1; i <= 20; i++) num[i] = i;
+    
+  for(int i = 1; i <= 10; i++) {
+    int a, b;
+    cin >> a >> b;        
+    reverse(a, b);
   }
-
-  for (int i = 0; i < 10; i++) {
-    // reverse cards
-    int start, end;
-    cin >> start >> end;
-    reverse(cards + start, cards + end + 1);
-  }
-
-  // print cards
-  for (int i = 1; i < n; i++) {
-    cout << cards[i] << ' ';
-  }
+    
+  for(int i = 1; i <= 20; i++) cout << num[i] << ' ';
 }

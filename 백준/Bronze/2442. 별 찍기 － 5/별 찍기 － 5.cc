0c19524a8aea90cf0818ff6477a817ce1d7ef746
ZergRocks <139047613+ZergRocks@users.noreply.github.com>
2024-08-25 18:40:13 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  // input
+  int n;
+  cin >> n;
+
+  // lines
+  for (int i = 0; i < n; i++) {
+    // space
+    for (int j = 0; j < n - i - 1; j++) {
+      cout << ' ';
+    }
+
+    // star
+    for (int j = 0; j < 2 * i + 1; j++) {
+      cout << '*';
+    }
+
+    cout << '\n';
+  }
+}
